ace1bf9a8582c1c69139b47bc6abac736e67f1c7
VRASHBH <sonivrashbh@gmail.com>
2026-07-06 02:02:15 +0530

    Added 0002-Add-Two-Numbers.cpp

--- backend/auto_push.py
+++ backend/auto_push.py
@@ -1,25 +1,55 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import subprocess
 import time
+import os
 
 class SolutionHandler(FileSystemEventHandler):
 
     def on_created(self, event):
-        if not event.is_directory:
-            print(f"[CREATED] {event.src_path}")
 
-    def on_modified(self, event):
-        if not event.is_directory:
-            print(f"[MODIFIED] {event.src_path}")
+        if event.is_directory:
+            return
+
+        if not event.src_path.endswith((".cpp", ".py", ".java")):
+            return
+
+        print(f"[NEW] {event.src_path}")
+
+        try:
+            subprocess.run(["git", "add", "."], check=True)
+
+            commit_message = f"Added {os.path.basename(event.src_path)}"
+
+            subprocess.run(
+                ["git", "commit", "-m", commit_message],
+                check=True
+            )
+
+            subprocess.run(
+                ["git", "push"],
+                check=True
+            )
+
+            print("✅ GitHub Updated")
+
+        except Exception as e:
+            print("❌ Error:", e)
+
 
 if __name__ == "__main__":
 
     observer = Observer()
-    observer.schedule(SolutionHandler(), "solutions", recursive=False)
+
+    observer.schedule(
+        SolutionHandler(),
+        "solutions",
+        recursive=False
+    )
 
     observer.start()
 
-    print("Watching solutions folder...")
+    print("🚀 Watching solutions folder...")
 
     try:
         while True:
